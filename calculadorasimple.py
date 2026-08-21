81d350a5d47daadd0bf5ddc8e3fa7dff797e5227
1rondel <danielenrique1806@gmail.com>
2026-08-21 00:07:20 -0400

    es una calculadora arithmetica simple, tiene una vulnerabilidad facil de detectar y aprovechar incluso por alguien con poca experiencia

---
+++ calculadorasimple.py
@@ -0,0 +1,4 @@
+#este programa es facil de hackear  porque no tiene validaciones de seguridad, es solo un ejemplo de como hacer una calculadora simple en python
+calculo = input('Ingrese la operación matematicaa realizar : ')
+resultado = eval(calculo)
+print('El resultado es :', resultado)
